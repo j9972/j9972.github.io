30657159ce41546db9739b8f7c5afa59ea95dfdc
j9972 <j9972@naver.com>
2019-11-15 17:12:29 +0900

    svg 이미지 보이게끔 div 태그안에 overflow: hidden 되어 있는거 지움

--- untitled1/sing.js
+++ untitled1/sing.js
@@ -1,8 +1,6 @@
 window.onload = function () {
     const lineUpMusic = document.getElementById('lineUpMusic');
-    //const t2 = document.getElementById('text_2');
     lineUpMusic.style.width = lineUpMusic.childElementCount * 500 + "px";
-    //t2.style.width = t2.childElementCount * 500 + "px";
 
     let index = 1051321560 + lineUpMusic.childElementCount;
 
@@ -10,12 +8,10 @@ window.onload = function () {
         --index;
         console.log(index);
         lineUpMusic.style.transform = "translateX(" + (index % lineUpMusic.childElementCount * -500) + "px)";
-        //t2.style.transform = "translateX(" + (index % t2.childElementCount * -500) + "px)";
     });
     document.getElementById('button_next').addEventListener('click', function () {
         ++index;
         console.log(index);
         lineUpMusic.style.transform = "translateX(" + (index % lineUpMusic.childElementCount * -500) + "px)";
-        //t2.style.transform = "translateX(" + (index % t2.childElementCount * -500) + "px)";
     });
 }

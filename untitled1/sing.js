1ce9b90ace6941f8ce7453c1b7af5dee763b4d26
j9972 <j9972@naver.com>
2019-12-02 21:08:25 +0900

    svg 이미지 보이게끔 div 태그안에 overflow: hidden 되어 있는거 지움

--- untitled1/sing.js
+++ untitled1/sing.js
@@ -1,6 +1,13 @@
-var swiper = new Swiper('.slider_first', {
-    navigation: {
-        nextEl: '.next',
-        prevEl: '.prev',
-    },
-});
+// var swiper = new Swiper('.slider_first', {
+//     navigation: {
+//         nextEl: '.next',
+//         prevEl: '.prev',
+//     },
+// });
+function on() {
+    document.getElementsByClassName("slider_first_photocollector").style.display = "block";
+}
+
+function off() {
+    document.getElementsByClassName("slider_first_photocollector").style.display = "none";
+}
